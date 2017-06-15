483de4b677862a0bdda90bfd3c918492a19786da
MICHAEL PATRICK JONATHAN REARDON <michaelpjreardon@gmail.com>
2017-06-15 18:50:32 +0100

    Add Detail action method

--- ComicGallery/ComicGallery/Controllers/ComicBooksController.cs
+++ ComicGallery/ComicGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller.";
+        }
     }
 }
