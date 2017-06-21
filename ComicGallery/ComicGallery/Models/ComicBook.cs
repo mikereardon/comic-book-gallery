48857dcb800158a7decde9d7e9668308d915bce1
MICHAEL PATRICK JONATHAN REARDON <michaelpjreardon@gmail.com>
2017-06-21 14:29:03 +0100

    Updated comic book model

--- ComicGallery/ComicGallery/Models/ComicBook.cs
+++ ComicGallery/ComicGallery/Models/ComicBook.cs
@@ -13,5 +13,18 @@ namespace ComicGallery.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        public string GetCoverImageFileName()
+        {
+            return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+        }
     }
 }
