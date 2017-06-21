19c430e22b4799d450480aeb87ffe4b0806399bf
MICHAEL PATRICK JONATHAN REARDON <michaelpjreardon@gmail.com>
2017-06-21 13:11:14 +0100

    Added Comic Book Detail view

--- ComicGallery/ComicGallery/Controllers/ComicBooksController.cs
+++ ComicGallery/ComicGallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,7 @@ namespace ComicGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
-            {
-                return Redirect("/");
-            };
-
-            return Content("Hello from the comic books controller.");
+            return View();
            
         }
     }
